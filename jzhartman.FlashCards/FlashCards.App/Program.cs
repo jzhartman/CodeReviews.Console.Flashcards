7eb5116171f739ee7b20e395dc9d27fcbd452e16
jzhartman <jzhartman@gmail.com>
2026-02-17 00:00:53 -0500

    Removed obsolete SQL porject Removed some important stuff from git ignore

---
+++ jzhartman.FlashCards/FlashCards.App/Program.cs
@@ -0,0 +1,31 @@
+﻿using FlashCards.Application;
+using FlashCards.ConsoleUI;
+using FlashCards.ConsoleUI.Controllers;
+using FlashCards.Infrastructure;
+using FlashCards.Infrastructure.Initialization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlashCards.App;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        var services = new ServiceCollection();
+        services.AddApplication();
+        services.AddInfrastructure(config);
+        services.AddConsoleUI();
+
+        var provider = services.BuildServiceProvider();
+        var initializer = provider.GetRequiredService<DbInitializer>();
+        initializer.Initialize();
+
+        var mainMenu = provider.GetRequiredService<MainMenuService>();
+        mainMenu.Run();
+    }
+}
